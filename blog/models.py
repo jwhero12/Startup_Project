51cd52cc33e0dc3ed7883df1859fe3d48a4199bc
Jiwon Han <jwhero12@naver.com>
2021-06-13 23:41:53 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,17 @@ class Category(models.Model):
         return f'/blog/category/{self.slug}/'
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories'
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=50)
+#     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+#
+#     def __str__(self):
+#         return self.name
+#
+#     def get_absolute_url(self):
+#         return f'/blog/tag/{self.slug}/'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -32,6 +42,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True,on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    #tags = models.ManyToManyField(Tag, blank=True)
+
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'

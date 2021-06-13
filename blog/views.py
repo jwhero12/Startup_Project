a47091a4088d15a1f41388d9ed23b0ed626ba0ae
Jiwon Han <jwhero12@naver.com>
2021-06-14 01:33:33 +0900

    태그 페이지 만들기:

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 class PostList(ListView):
     model = Post
@@ -12,24 +12,6 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-    def category_page(request, slug):
-        if slug == 'no_category':
-            category = '미분류'
-            post_list = Post.objects.filter(category=None)
-        else:
-            category = Category.objects.get(slug=slug)
-            post_list = Post.objects.filter(category=category)
-
-        return render(
-            request,
-            'blog/post_list.html',
-            {
-                'post_list': post_list,
-                'categories': Category.objects.all(),
-                'no_category_post_count': Post.objects.filter(category=None).count(),
-                'category': category,
-            }
-        )
 
 class PostDetail(DetailView):
     model = Post
@@ -40,6 +22,40 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
     #template_name = 'blog/post_list.html'
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
 
 
 # def index(request):
